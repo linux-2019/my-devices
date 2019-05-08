b21ac8e0b1d191dd35d0a97e02a719031ee53089
Nico De Witte <nico.dewitte@outlook.com>
2019-05-08 19:51:51 +0200

    Make mac address unique

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,4 +1,5 @@
 class Device < ApplicationRecord
-  validates :mac, format: { with: /\A([0-9A-F]{2}[-:]){5}([0-9A-F]{2})\z/ }, presence: true
+  validates :mac, format: { with: /\A([0-9A-F]{2}[-:]){5}([0-9A-F]{2})\z/ },
+    presence: true, uniqueness: true
   has_many :updates
 end
