d813bc4dfb46c65ac442f33481480ab8a6379e69
Nico De Witte <nico.dewitte@outlook.com>
2019-05-08 19:30:14 +0200

    Add basic model for a device

---
+++ db/migrate/20190508172913_create_devices.rb
@@ -0,0 +1,10 @@
+class CreateDevices < ActiveRecord::Migration[5.2]
+  def change
+    create_table :devices do |t|
+      t.string :mac
+      t.string :name
+
+      t.timestamps
+    end
+  end
+end
