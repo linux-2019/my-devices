2123f5dc550970115b88cd429c9689d7b85ffc35
Nico De Witte <nico.dewitte@outlook.com>
2019-05-08 19:41:28 +0200

    Add basic validation of mac and ip

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,2 +1,3 @@
 class Device < ApplicationRecord
+  validates :mac, format: { with: /\A([0-9A-F]{2}[-:]){5}([0-9A-F]{2})\z/ }
 end
--- app/models/update.rb
+++ app/models/update.rb
@@ -1,2 +1,5 @@
+require "resolv"
+
 class Update < ApplicationRecord
+  validates :ip_address, format: { with: Resolv::IPv4::Regex }
 end
