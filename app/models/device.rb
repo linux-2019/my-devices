7627df277c71bea56bfc8a52d73a29de04561863
Nico De Witte <nico.dewitte@outlook.com>
2019-05-08 20:35:24 +0200

    Fix mac always being saved lowercase

--- app/models/device.rb
+++ app/models/device.rb
@@ -2,4 +2,10 @@ class Device < ApplicationRecord
   validates :mac, format: { with: /\A([0-9A-F]{2}[-:]){5}([0-9A-F]{2})\z/ },
     presence: true, uniqueness: true
   has_many :updates
+
+  before_save :downcase_mac
+  
+  def downcase_mac
+    self.mac.downcase!
+  end
 end
