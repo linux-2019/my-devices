2a796361cafeeae52640080b2887aaf27cd14fe8
Nico De Witte <nico.dewitte@outlook.com>
2019-05-08 19:44:37 +0200

    Add model relationships

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,3 +1,4 @@
 class Device < ApplicationRecord
-  validates :mac, format: { with: /\A([0-9A-F]{2}[-:]){5}([0-9A-F]{2})\z/ }
+  validates :mac, format: { with: /\A([0-9A-F]{2}[-:]){5}([0-9A-F]{2})\z/ }, presence: true
+  has_many :updates
 end
--- app/models/update.rb
+++ app/models/update.rb
@@ -1,5 +1,6 @@
 require "resolv"
 
 class Update < ApplicationRecord
-  validates :ip_address, format: { with: Resolv::IPv4::Regex }
+  validates :ip_address, format: { with: Resolv::IPv4::Regex }, presence: true
+  belongs_to :device
 end
